f7dfd0be036e6b4190ac6fe0c1f5331df43d67f0
Mannyplg <pettigrew.i@gmail.com>
2022-12-05 06:19:06 -0800

    week 5 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,6 +14,25 @@
 
 // a) Create a test with expect statements using the variables provided.
 
+// describe("codedMessage", () => {
+//     it("takes in a string and returns a string with a coded message", () => {
+//       expect(codedMessage(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+//       expect(codedMessage(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+//       expect(codedMessage(secretCodeWord3)).toEqual("3cc3ntr1c")
+
+//     })
+//   })
+
+//   const codedMessage = (str) => {
+//     // .replaceAll()string method returns , with g, global indicator, will replace 
+//     // all specificied occurrences and return replacement value.
+//     return str.replaceAll(/a/g, '4')
+//               .replaceAll(/e/g, '3')
+//               .replaceAll(/i/g, '1')
+//               .replaceAll(/o/g, '0')
+//               .replaceAll(/E/g, '3')
+//   }
+
 const secretCodeWord1 = "Lackadaisical"
 // Expected output: "L4ck4d41s1c4l"
 const secretCodeWord2 = "Gobbledygook"
@@ -23,18 +42,54 @@ const secretCodeWord3 = "Eccentric"
 
 // b) Create the function that makes the test pass.
 
+// Created function codedMessage, which takes in a string (str)
+// Input: String variable
+// Output: The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
+// .replaceAll()string method returns , with g, global indicator, will replace  
+// all specificied occurrences and return replacement value.
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
 
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+describe("containingLetter", () => {
+    it("returns an array of all the words containing that particular letter", () => {
 
-const letterA = "a"
-// Expected output: ["Mango", "Apricot", "Peach"]
-const letterE = "e"
-// Expected output: ["Cherry", "Blueberry", "Peach"]
+     const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+     const letterA = "a"
+    const expectedOutput1 = ["Mango", "Apricot", "Peach"]
+    const letterE = "e"
+    const expectedOutput2 = ["Cherry", "Blueberry", "Peach"]
 
+      expect(containingLetter(fruitArray, letterA)).toEqual(expectedOutput1)
+      expect(containingLetter(fruitArray, letterE)).toEqual(expectedOutput2)
+
+    })
+})
+    // created a function takes an array and str as parameters 
+  const containingLetter = (arr, str) => {
+    // use .map()method to lowercase all words in the array
+        let tolowerCaseArray = arr.map((value => value.toLowerCase()))
+        //use .filter() method to find words in that contain identified string 
+        let searchstrArray   = tolowerCaseArray.filter((value => value.includes(str)))
+   //  use .map to uppercase the letter of each value which are lowercased
+   // and used .substr() method to provide rest letters in lowercased.
+        let toupperCaseArray = searchstrArray.map((value => value[0].toUpperCase() + value.substr(1)))
+        return toupperCaseArray
+    // Stored each iteration into its own variable with stored information.
+    // Each iteration references previous array.     
+  }
+ 
 // b) Create the function that makes the test pass.
+
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 total
+
+// 27 Nov.--> Test Suites: 1 passed, 1 total
+//            Tests:       1 passed, 1 total
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
@@ -50,3 +105,7 @@ const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
 
 // b) Create the function that makes the test pass.
+// function list() {
+//     return slice(arguments);
+//   }
+// The slice() method returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) where start and end represent the index of items in that array. The original array will not be modified.
--- interview-questions.md
+++ interview-questions.md
@@ -8,44 +8,44 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What does calling super do in a class?
 
-Your answer:
+Your answer: When calling a super, we ask that our new thing we're making, have an similiar outline as a standard feature.
 
-Researched answer:
+Researched answer: Keyword; calls on a parent class with the same name as the method thatb calls super...(ref.rubyguides.com) 
 
-2. What is a gem?
+2. What is a gem? 
 
-Your answer:
+Your answer: Similiar to Node.js, Ruby has packages or libraries that can be used to provide common functions in a program.
 
-Researched answer:
+Researched answer: Libraries that contain Ruby code that allows a programmer to use the code within the gem in their own program, without inserting that code...(www.medium.com)
 
 3. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: Making sense of information from data listed on row, tables , an or columns. there are other databases such as MongoDB, HarperDb, etc...
 
-Researched answer:
+Researched answer: Relational database establishes links between information by joining tables, which makes it easy to understand and gain insights about the relationship between variour data points...(www.cloud.google.com)
 
 4. What are primary keys? Why are they important?
 
-Your answer:
+Your answer: Primary keys are unique identifiers for each value in a database table.
 
-Researched answer:
+Researched answer: a database must have a primary key for accessing and modifying data. Primary key is the column or row that contain unique values...(www.ibm.com)
 
 5. What are the HTTP verbs? What is each verb's corresponding CRUD action?
 
-Your answer:
+Your answer: The Hypertext Transfer Protocol verbs are GET, POST, PUT, PATCH, and DELETE. GET- read/display. POST- add new content, a create action. PUT- replaces content-update action. PATCH- replaces pieces of content, update action. DELETE- deletes and removes content action.
 
-Researched answer:
+Researched answer: In computer programming, create, read, update, and delete are four basic operations of persistent storage...(www.wikipedia.com)
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: A set a standards used in many different languages to create effecient, reusable routes...(www.learnhowtoprogram.com)
 
-2. Ruby on Rails:
+2. Ruby on Rails: Server side web application framework.
 
-3. Database migrations:
+3. Database migrations: File generated through Rails command that will allow us to make changes to the database. Edit databases
 
-4. Params:
+4. Params: refers to parameters being passed to the controller via GET or POST request...(www.flatironedschool.com)
 
-5. ERB:
+5. ERB: ERB is a template engine. It allows us to mix HTML and Ruby to generate web pages using data from our database...(www.rubyguides.com)
